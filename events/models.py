2a0dbc56a272970ff2ec6ed620f6fe815d7a16ea
dravog7 <dravog77@gmail.com>
2020-02-13 21:23:57 +0530

    added dev requirements and event model init

--- events/models.py
+++ events/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class event(models.Model):
+    name = models.CharField()
+    description = models.TextField()
+    reglink = models.URLField()
+    prize = models.IntegerField()
+    contacts = models.TextField() #to easily add text. its JSON
+    image = models.ImageField()
