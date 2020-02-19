58e915d1d736c55969f7998e26ef1ff719bedc15
dravog7 <dravog77@gmail.com>
2020-02-19 23:15:48 +0530

    fixed azure

--- backend/custom_azure.py
+++ backend/custom_azure.py
@@ -1,7 +1,7 @@
 from storages.backends.azure_storage import AzureStorage
 from os import environ
-account_key = environ.get('ACCOUNT_KEY','testhttp1234')
-account_name = environ.get('ACCOUNT_NAME','wkySBGV/zD9LZ15JYf5vxFUvQK4DCe7FkYDgnP9MRaeNne4lgmZF3yMnxH5+ky9UsJKPbohpsiyn5B8Gmz8NDA==')
+account_name = environ.get('ACCOUNT_NAME','testhttp1234')
+account_key = environ.get('ACCOUNT_KEY','wkySBGV/zD9LZ15JYf5vxFUvQK4DCe7FkYDgnP9MRaeNne4lgmZF3yMnxH5+ky9UsJKPbohpsiyn5B8Gmz8NDA==')
 class AzureMediaStorage(AzureStorage):
     account_name = account_name
     account_key = account_key
