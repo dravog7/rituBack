865ac7363c6d141ca69e792f33befea3317716a9
dravog7 <dravog77@gmail.com>
2020-02-20 23:13:44 +0530

    restrict admin page edit access

--- events/admin.py
+++ events/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from .models import event
 # Register your models here.
-admin.site.register(event)
+@admin.register(event)
+class eventAdmin(admin.ModelAdmin):
+    def save_model(self,request,obj,form,change):
+        obj.user = request.user
+        super(eventAdmin,self).save_model(request,obj,form,change)
+    
+    def get_queryset(self,request):
+        qs = super(eventAdmin,self).get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(user = request.user)
--- events/models.py
+++ events/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class event(models.Model):
     DEPTS = [
@@ -19,6 +19,6 @@ class event(models.Model):
     contacts = models.TextField() #to easily add text. its JSON
     image = models.ImageField()
     dept = models.CharField(choices=DEPTS,max_length=10)
-
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
     def __str__(self):
         return self.name
